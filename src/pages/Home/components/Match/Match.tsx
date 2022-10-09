a8f6ddbafa06a9bb78785637a2c2f3bcfd948c91
Gabriel Ramos <gabramsua@gmail.com>
2022-10-09 10:43:26 +0200

    Match styling approach v1

--- src/pages/Home/components/Match/Match.tsx
+++ src/pages/Home/components/Match/Match.tsx
@@ -5,9 +5,17 @@ import { Match as Game} from "@/models/models";
 export interface MatchInterface {}
 
 const Match = ({ game }: { game: Game }) => {
-	return <div className='match'>
-		{game.homeTeam} - {game.awayTeam}
-	</div >;
+	return (
+	<div className='match'>
+		<div className="match-team">
+			<img className='match-flag' src={game.homeFlag} /><span>{game.homeTeam}</span>
+		</div>
+			
+		<div className="match-team">
+			<img className='match-flag' src={game.awayFlag} /><span>{game.awayTeam}</span>
+		</div>
+		<button>PLAY</button>
+	</div >);
 };
 
 export default Match;
